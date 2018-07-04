18cf4cb168680ddbf06298fc434fec2b2e168f76
Raymond Wu <virus.warnning@gmail.com>
2018-07-04 17:12:19 +0800

    Improve barrier sample.
    Mentions: #3

--- 05_barrier.py
+++ 05_barrier.py
@@ -6,9 +6,11 @@ def main():
     CL_CODE = '''
     kernel void barrier_test(local int* temp, int with_barrier) {
         int lid = get_local_id(0);
+        size_t lsz = get_local_size(0);
 
         if (lid % 2 == 1) {
-            for (int n = 0; n < 100 * lid; n++) {
+            int loop = lid * lid;
+            for (int n = 0; n < loop; n++) {
                 printf("\\r");
             }
         }
@@ -20,28 +22,26 @@ def main():
 
         if (lid == 0) {
             int sum = 0;
-            for (int i=0; i<5; i++) {
-                printf("temp[%d]=%d\\n", i, temp[i]);
+            for (size_t i=0; i<lsz; i++) {
                 sum += temp[i];
             }
-            printf("sum=%d\\n", sum);
+            printf("sum[0..%d]=%d\\n", lsz, sum);
         }
     }
     '''
 
-    os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
-
     plf = [(cl.context_properties.PLATFORM, cl.get_platforms()[0])]
-    ctx = cl.Context(dev_type=cl.device_type.CPU, properties=plf)
+    ctx = cl.Context(dev_type=cl.device_type.GPU, properties=plf)
     prg = cl.Program(ctx, CL_CODE).build()
     queue = cl.CommandQueue(ctx)
 
-    temp = cl.LocalMemory(5 * 4)
+    ITEM_COUNT = 100
+    temp = cl.LocalMemory(ITEM_COUNT * 4)
     print('===== Run without barrier =====')
-    prg.barrier_test(queue, (5,), (5,), temp, np.int32(0))
+    prg.barrier_test(queue, (ITEM_COUNT,), (ITEM_COUNT,), temp, np.int32(0))
     queue.finish()
     print('===== Run with barrier =====')
-    prg.barrier_test(queue, (5,), (5,), temp, np.int32(1))
+    prg.barrier_test(queue, (ITEM_COUNT,), (ITEM_COUNT,), temp, np.int32(1))
     queue.finish()
 
 if __name__ == '__main__':
