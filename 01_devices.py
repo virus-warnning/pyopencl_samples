6dcff92b7d227be63cc09daa13edbb0bbfb44058
Raymond Wu <virus.warnning@gmail.com>
2018-07-13 18:51:28 +0800

    Add max clock frequency and resort device infomations.
    Mentions: #10

--- 01_devices.py
+++ 01_devices.py
@@ -18,16 +18,18 @@ def main():
             # Show primary informations.
             d_cu   = d.get_info(cl.device_info.MAX_COMPUTE_UNITS)
             print('   |- Max Compute Units: {}'.format(d_cu))
-            d_gmsz = d.get_info(cl.device_info.GLOBAL_MEM_SIZE)
-            print('   |- Max Global Memory Size: {}MB'.format(d_gmsz >> 20))
-            d_lmsz = d.get_info(cl.device_info.LOCAL_MEM_SIZE)
-            print('   |- Max Local Memory Size: {}KB'.format(d_lmsz >> 10))
             d_wgsz = d.get_info(cl.device_info.MAX_WORK_GROUP_SIZE)
             print('   |- Max Work Group Size: {}'.format(d_wgsz))
             d_widm = d.get_info(cl.device_info.MAX_WORK_ITEM_DIMENSIONS)
             print('   |- Max Work Item Dimensions: {}'.format(d_widm))
             d_wisz = d.get_info(cl.device_info.MAX_WORK_ITEM_SIZES)
             print('   |- Max Work Item Size: {}'.format(d_wisz))
+            d_lmsz = d.get_info(cl.device_info.LOCAL_MEM_SIZE)
+            print('   |- Max Local Memory Size: {}KB'.format(d_lmsz >> 10))
+            d_gmsz = d.get_info(cl.device_info.GLOBAL_MEM_SIZE)
+            print('   |- Max Global Memory Size: {}MB'.format(d_gmsz >> 20))
+            d_freq = d.get_info(cl.device_info.MAX_CLOCK_FREQUENCY)
+            print('   |- Max Clock Frequency: {}MHz'.format(d_freq))
 
             # List extensions.
             print('   |- Extensions: ')
